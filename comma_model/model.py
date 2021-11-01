526fa8cc422d72959bcaaa0fca5d129b2291e50b
Gautam Kumar Jain <gautamku@rocket.hpc.ut.ee>
2021-11-01 13:31:39 +0200

    initial commit for comma model

---
+++ comma_model/model.py
@@ -0,0 +1,32 @@
+import torch 
+import torch.nn as nn 
+import torch.functional as F
+import torchvision 
+import numpy as np 
+
+class InitialBlock(nn.Module):
+    def __init__(self):
+        super(InitialBlock, self).__init__
+        
+
+    def forward(self, x):
+
+        pass 
+
+
+class BottleneckBlock1_3_1(nn.Module):
+    def __init__(self):
+        super(BottleneckBlock1_3_1).__init__
+
+
+
+    def forward(self, x):
+        pass 
+
+
+class BottleneckBlock1_5_1(nn.Module):
+    def __init(self):
+        super(BottleneckBlock1_5_1, self).__init__
+
+    def forward(self,x):
+        pass 
