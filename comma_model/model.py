35db5c7b0506fc41235ea3aa3b0976b8f7e9d7ce
gautamjain1009 <gautamjain1009@gmail.com>
2021-11-01 16:24:47 +0200

    drafting residual layers

--- comma_model/model.py
+++ comma_model/model.py
@@ -5,9 +5,16 @@ from torch.nn.modules.activation import ELU
 from torch.nn.modules.linear import Identity
 import torchvision 
 import numpy as np 
+
 """
-Things to do change relu ---> ELU 
+Things to do change relu ---> ELU
+
+I have figured it out that there is expansion factor of 6 in all the next residual layers
+[16,24,48,88,120,208,352] * 6 == num of filters in the respective residual layers
+
 """
+
+#starting residual block
 class InitialBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(InitialBlock, self).__init__
@@ -21,13 +28,13 @@ class InitialBlock(nn.Module):
     def forward(self, x):
         
         identity = x.clone()
-        x = self.relu(self.batch_norm1(self.conv1))
-        x = self.relu(self.batch_norm2(self.conv2))
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.relu(self.batch_norm2(self.conv2(x)))
         x+= identity
 
         return x
 
-
+#repititive residual block with (1x1) - (3x3) - (1x1) kernel
 class BottleneckBlock1_3_1(nn.Module):
     def __init__(self, in_channels, out_channels, expansion):
         super(BottleneckBlock1_3_1).__init__
@@ -40,24 +47,67 @@ class BottleneckBlock1_3_1(nn.Module):
         self.batch_norm2 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
         self.batch_norm3 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
 
-
-
     def forward(self, x):
-        
-        pass 
 
+        identity = x.clone()
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+        x = self.relu(self.batch_norm3(self.conv3(x)))
+        x+=identity
+        return x 
 
+# reptitive block with (1x1) - (5x5) - (1x1) kernel
 class BottleneckBlock1_5_1(nn.Module):
-    def __init(self):
+    def __init(self,in_channels, out_channels, expansion):
         super(BottleneckBlock1_5_1, self).__init__
 
+        self.expansion = expansion
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride =1, padding =1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride =1, padding =1)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride =1, padding =1)
+        self.relu = nn.ReLU()
+        self.batch_norm1 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
+        self.batch_norm3 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
     def forward(self,x):
-        pass 
+
+        identity = x.clone()
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+        x = self.relu(self.batch_norm3(self.conv3(x)))
+        x+=identity
+        return x 
+ 
 
 class AggregationBlock(nn.Module):
-    def __init__(self):
+    def __init__(self,in_channels, out_channels, expansion):
         super(AggregationBlock, self).__init__
-
+        self.expansion = expansion
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride =1, padding =1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride =1, padding =1)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride =1, padding =1)
+        self.relu = nn.ReLU()
+        self.batch_norm1 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)
+        self.batch_norm3 = nn.BatchNorm2d(out_channels, eps = 0.001, momemtum = 0.99)          
 
     def forward(self,x):
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+        x = self.batch_norm3(self.conv1(x))
+
+        return x 
+
+
+## Combined Resnet for conv features extraction
+
+class ConvFeatureExtractor(nn.Module):
+    def __init__(self):
+        super(ConvFeatureExtractor,self).__init__
+
+
+    def make_layers(self, ResBlock):
+        pass 
+
+    def feed_forward_network(self):
         pass
